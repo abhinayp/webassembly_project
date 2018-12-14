40d1f0e665f9b4bfa88048a5b3c32ce45ef939ba
mansiboriya <mansiboriya@gmail.com>
2018-12-13 20:38:59 -0500

    class traplist

---
+++ model/TrapList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TrapList
+{
+
+    public TrapList()
+    {
+    }
+
+    public Trap[] generateTraps()
+    {
+
+        Random rnd = new Random();
+        int numOfTraps = rnd.Next(15, 26);
+        Trap[] trapList = new Trap[numOfTraps];
+
+        for (int i = 0; i <= numOfTraps; i++)
+        {
+            int x = rnd.Next(0, 15);
+            int y = rnd.Next(0, 15);
+            Trap t = new Trap();
+            t.x = x;
+            t.y = y;
+            trapList[i] = t;
+            Console.WriteLine(trapList);
+        }
+
+
+        return trapList;
+    }
+}
